7dff5eb8e64c266f1300b3a94fa2d6418a2fb826
Maciek Matuszewski <maciek.m2004@wp.pl>
2025-08-18 20:34:20 +0200

    documents page done

--- example-app/resources/views/documents.blade.php
+++ example-app/resources/views/documents.blade.php
@@ -117,7 +117,7 @@
             You can read more on <a href="https://lifeindenmark.borger.dk/settle-in-denmark/ics-international-citizen-service/ics-west-in-aarhus?frm2=micro-articles-list-f180e8a0-6f4e-47b8-aa8b-4e55284d7bb8" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">ICS West website.</a> Follow steps there.
             <ul class="ps-5 mt-2 space-y-1 list-inside list-disc">
             </ul>
-            <p class="text-gray-500 whitespace-normal dark:text-gray-400">As far as we remember (it may be old way), you could have booked a meeting on a <a href="https://horsens.dk/workandstudy/newtohorsens/officialmatters" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">Horsens Kommune</a> website and go there to get your CPR number, which would make you avoid taking a trip to Aarhus. You would need to call them and ask tho.</p>
+            <p class="text-gray-500 whitespace-normal dark:text-gray-400">As far as we remember (it may be old way), you could have booked a meeting on a <a href="https://horsens.dk/workandstudy/newtohorsens/officialmatters" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">Horsens Kommune</a> website and go there to get your CPR number, which would make you avoid taking a trip to Aarhus. Or just go there without an appointment and stand in a queue for a while.</p>
         </li>
         <li>
             The whole applying process will probably take 1-2 hours or so, therefore make sure to have all your documents ready. Then, you will receive a CPR number on the spot, which is great but <p class="font-semibold text-gray-900 underline dark:text-black decoration-red-500">you won't get your Yellow Card right away. Waiting for Yellow Card is approximately 1-3 weeks.</p>
@@ -167,7 +167,8 @@
 
     <h2 class="text-2xl font-bold dark:text-neutral-900">Tax <mark class="px-2 text-white bg-blue-600 rounded-sm dark:bg-blue-500">cards</mark> in a nutshell:</h2>
     <br>
-    <p class="pl-6 space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4 text-base">Tax card is a generic term for primary tax card (hovedkort), secondary tax card (bikort) and tax exemption card (frikort). You can read about how to get them in detail <a href="https://skat.dk/en-us/businesses/employees-and-pay/non-danish-labour/get-a-tax-card-as-a-non-danish-employee?oid=2244407&vid=0" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">here.</a></p>
+    <p class="pl-6 space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4 text-base">Tax card is a generic term for primary tax card (hovedkort), secondary tax card (bikort) and tax exemption card (frikort). You can read about how to get them in detail <a href="https://skat.dk/en-us/businesses/employees-and-pay/non-danish-labour/get-a-tax-card-as-a-non-danish-employee?oid=2244407&vid=0" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">here.</a> While applying they will ask you for approx. year income and so on, so be prepared.
+        Don't worry, any amounts put too high will be <span class="font-semibold text-gray-900 underline dark:text-black decoration-indigo-500">returned in TAX return on March.</span> Better put it a bit higher than actual year income to be sure you chill.</p>
     <p class="pl-6 space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4 text-base"><strong>We recommend you don’t apply for your tax card until 1 month before you start your job,</strong> as they simply might not manage to get it for you before you start work. With that said, once you are <i><strong>sure</strong></i> you will get it, apply right away.</p>
     <p class="pl-6 space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4 text-base">By default, you set it to receive your salary on a hovedkort(A card) and put SU on bikort(B card). We mean by default, because, usually the monthly salary is bigger than SU, which means if we want the highest income with the lowest paid in TAX,
         <span class="font-semibold text-gray-900 underline dark:text-black decoration-indigo-500">we put the HIGH income on A card and LOWER income on B card, so the lower income gets taxed the most and high income the lowest.</span>  </p>
@@ -175,7 +176,7 @@
         <li>
             <span class=" font-bold">Primary tax card (hovedkort)</span> states both the amount you will not be taxed on (your personal allowance) and the tax rate you will have to pay.
             <ul class="ps-5 mt-2 space-y-1 list-inside list-disc">
-                <li>Your personal tax rate is calculated based on an 8% labor market tax, average municipal tax of ~25.068%, and progressive state taxes of 12.01% and 15%. TLDR; Approx. 27% TAX</li>
+                <li>Your personal tax rate is calculated based on an 8% labor market tax, average municipal tax of ~25.068%, and progressive state taxes of 12.01% and 15%. <p class="font-semibold text-gray-900 underline dark:text-black decoration-red-500">TLDR; Approx. 20-30% of your salary will be TAXED</p></li>
             </ul>
         </li>
         <li>
@@ -194,10 +195,13 @@
         </li>
     </ul>
 
+    <p class="pl-6 space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4 text-base">Calculate your expected profit <a href="https://salaryaftertax.com/salary-calculator/denmark" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">here.</a></p>
+
     <!-- SU -->
     <div class="bg-white rounded-2xl shadow p-6 mb-10 dark:bg-neutral-100" id="su">
     <p class="font-bold mb-3 text-neutral-700 md:text-2xl dark:text-neutral-900">SU (Danish state educational grant)</p>
-    <p class="text-gray-500 dark:text-neutral-700">SU stands for Statens Uddannelsesstøtte, which is the Danish State Educational Grant and Loan Scheme. It is a financial aid program that provides financial support to eligible students studying at institutions of higher education in Denmark.</p>
+        <p class="text-gray-500 dark:text-neutral-700"><span class="font-semibold text-gray-900 underline dark:text-black decoration-indigo-500">This is the sauce.</span> SU stands for Statens Uddannelsesstøtte, which is the Danish State Educational Grant and Loan Scheme. It is a financial aid program that provides financial support to eligible students studying at institutions of higher education in Denmark.</p>
+        <p class="text-gray-500 dark:text-neutral-700"> You will receive approx. 1000$ per month. So, to put it into perspective its <strong>7000DKK before tax - 4500DKK after tax (SU on B tax card).</strong></p>
 
     <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
         <div class="mb-3">
@@ -215,7 +219,42 @@
                     <svg class="shrink-0 w-3.5 h-3.5 text-green-700 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
                     </svg>
-                    <span>Have been working in Denmark for at least 10-12 hours per week</span>
+                    <span>Have been/will be working in Denmark for at least 10-12 hours per week. (Just be sure to have 40h total per month)</span>
+            </ul>
+            <ul class="space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4">
+                <li class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg class="shrink-0 w-3.5 h-3.5 text-green-700 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                    </svg>
+                    <span>You don't receive any other public benefits (I believe you can still apply for Accommodation Support tho)</span>
+            </ul>
+            <ul class="space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4">
+                <li class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg class="shrink-0 w-3.5 h-3.5 text-green-700 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                    </svg>
+                    <span>MitID</span>
+            </ul>
+            <ul class="space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4">
+                <li class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg class="shrink-0 w-3.5 h-3.5 text-green-700 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                    </svg>
+                    <span>NemKonto account</span>
+            </ul>
+            <ul class="space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4">
+                <li class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg class="shrink-0 w-3.5 h-3.5 text-green-700 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                    </svg>
+                    <span>DigitalPost account (e-boks)</span>
+            </ul>
+            <ul class="space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4">
+                <li class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <svg class="shrink-0 w-3.5 h-3.5 text-green-700 dark:text-green-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
+                    </svg>
+                    <span>Equal citizen status document</span>
             </ul>
         </div>
     </div>
@@ -224,21 +263,28 @@
     <br>
     <ul class="pl-6 space-y-4 text-left text-gray-500 dark:text-gray-800 mb-4 list-decimal text-base">
         <li>
-            fuck do i know
+            Be sure you have your Equal citizen status document ready.
             <ul class="ps-5 mt-2 space-y-1 list-inside list-disc">
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>If not, create minSU account </li>
+                <li>Go to <a href="https://lifeindenmark.borger.dk/ActionPage?selfserviceId=6881042e-3cc5-4e19-abc8-c44834eab8da&referringPageId=b4385e97-c278-4a61-ba15-d1280756f661&type=DK&cookiebanner=true" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">official SU website</a></li>
+                <li>Follow their instructions.</li>
             </ul>
         </li>
         <li>
-
+            Be sure you have the job.
             <ul class="ps-5 mt-2 space-y-1 list-inside list-disc">
-                <li></li>
-                <li></li>
-                <li></li>
+                <li>You need to have proof of employment.</li>
+                <li>You will know if you can (you will) be granted SU based on monthly income. <span class="font-semibold text-gray-900 underline dark:text-black decoration-red-500">Don't exceed 19000DKK before tax per month to avoid receiving SU return notice. </span></li>
+            </ul>
+        </li>
+        <li>
+            With everything ready, you can <a href="https://lifeindenmark.borger.dk/ActionPage?selfserviceId=6881042e-3cc5-4e19-abc8-c44834eab8da&referringPageId=b4385e97-c278-4a61-ba15-d1280756f661&type=DK&cookiebanner=true" class="font-medium text-blue-600 underline dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700 hover:no-underline">apply for SU.</a>
+            <ul class="ps-5 mt-2 space-y-1 list-inside list-disc">
+                <li><strong>IN CASE</strong>, you don't have any savings/income/collectibles/money on you, make sure to address that in the SU form, so you get the highest amount. (Only in case if you don't have them, of course...)</li>
             </ul>
         </li>
     </ul>
+
+    <p class="text-gray-500 dark:text-neutral-700">We recommend you apply for SU <span class="font-semibold text-gray-900 underline dark:text-black decoration-indigo-500">right after you get the employment contract.</span> It is due to long waiting times to get a response <strong>  (approx 2-4 weeks)</strong> and I assume we want to get the SU ASAP.</p>
 
 </x-inside-layout>
