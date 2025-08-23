25f5bd867377813793255d3f93a1d2695af5f12e
skaff official <polski.adam300@gmail.com>
2025-08-23 15:50:35 +0200

    minor tweaks

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -13,7 +13,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/banking">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Banking recommendations</h5>
         </a>
     <p class="mb-3 font-normal text-gray-400">Here is an overview of everything bank related, from which banks we recommend to payment types in Denmark.</p>
@@ -25,7 +25,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/work">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Everything about work</h5>
         </a>
     <p class="mb-3 font-normal text-gray-400">This section will talk about estimated (by experience) student earnings for a part-time work and where to look for jobs.</p>
@@ -37,7 +37,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/accommodation">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Accommodation tips & info</h5>
         </a>
     <p class="mb-3 font-normal text-gray-400">Handful piece of information on how to find a new place, where to apply for Accommodation support, what to look out for, what are the prices, all hidden hooks.</p>
@@ -49,7 +49,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/commuting">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Commuting explained</h5>
         </a>
     <p class="mb-3 font-normal text-gray-400">Commuting in Denmark is pretty straightforward and well developed. All info about having a car, bike, scooter, using DSB, Midtraffiken, Resjekort, Commute card etc. </p>
@@ -61,7 +61,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/social">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Feeling social today?</h5>
         </a>
     <p class="mb-3 font-normal text-gray-400">Here we will drop places where you can meet new people. From apps like Goin' to Facebook groups and culture.</p>
@@ -73,7 +73,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/food">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Food & shopping</h5>
         </a>
         <p class="mb-3 font-normal text-gray-400">Denmark might feel pricey at first, but here we will cover a lot of places that enable you to be cost-effective with your
@@ -86,7 +86,7 @@
         </a>
     </div>
     <div class="max-w-xl max-h p-6 bg-gray-800 border border-gray-700 rounded-lg shadow-sm">
-        <a href="#">
+        <a href="/lifestyle">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white">Lifestyle</h5>
         </a>
     <p class="mb-3 font-normal text-gray-400">Here is some information about finding your doctor and booking an appointment but also where to play sports, find interest clubs, where to go clubbing, spend free time or places you must check out.</p>
--- example-app/resources/views/work.blade.php
+++ example-app/resources/views/work.blade.php
@@ -153,7 +153,7 @@
         <div class="md:flex">
             <ul class="flex-column space-y space-y-4 text-sm font-medium text-gray-400 dark:text-gray-400 md:me-4 mb-4 md:mb-0" id="feriepenge-tabs">
                 <li>
-                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="where-to-get" aria-current="page">
+                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 text-white bg-blue-600 dark:bg-blue-600 active w-full" data-tab="where-to-get" aria-current="page">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-8 pr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
                         </svg>
@@ -161,7 +161,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="how-to-claim">
+                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="how-to-claim">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7 pr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
                         </svg>
@@ -169,7 +169,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="how-much">
+                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="how-much">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7 pr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
                         </svg>
@@ -177,7 +177,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="be-aware">
+                    <a href="#" class="tab-link inline-flex items-center px-4 py-3 rounded-lg hover:text-white bg-gray-800 hover:bg-gray-700 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white" data-tab="be-aware">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7 pr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m0-10.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.25-8.25-3.286Zm0 13.036h.008v.008H12v-.008Z" />
                         </svg>
